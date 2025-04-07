a1d0108edc3515407e62c095c4f047dbc92fa0ee
whaff <109013631+whaff@users.noreply.github.com>
2025-04-07 17:05:11 +0300

    Add files via upload

---
+++ Hospital Management System/src/model/DoctorAssignment.java
@@ -0,0 +1,11 @@
+// TO-DO @turki here you can start doing doctors assignment
+
+/*  package src.ds;
+
+ import src.model.Doctor;
+ import java.util.HashMap;
+
+ public class DoctorAssignment {
+    
+ }
+*/
